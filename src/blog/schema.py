40dac677aea085ba7682316500d0493f4fa693b8
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-04-11 15:05:00 +0100

    Define query and mutations for Blog model

---
+++ src/blog/schema.py
@@ -0,0 +1,42 @@
+import strawberry
+from typing import List
+from .models import BlogPost
+from .types import BlogPostType
+
+# Query
+# equivalent to Read operation and GET method
+@strawberry.type
+class Query:
+    @strawberry.field
+    def get_blogposts(self, title:str=None) -> List[BlogPostType]:
+        if title:
+            post = BlogPost.objects.filter(title=title)
+            return post
+        return BlogPost.objects.all()
+    
+# Mutation
+# equivalent to `Create, Update, Delete` operations
+# and `POST, PUT, PATCH, DELETE` methods
+@strawberry.type
+class Mutation:
+    @strawberry.field
+    def create_blogpost(self, title:str, author:str, message:str) -> BlogPostType:
+        blog = BlogPost(
+            title = title,
+            author = author,
+            message = message
+        )
+        blog.save()
+        return blog
+    
+    @strawberry.field
+    def update_blogpost(self, id:int, title:str, author:str, message:str) -> BlogPostType:
+        blog = BlogPost.objects.get(id=id)
+        blog.title = title
+        blog.author = author
+        blog.message = message
+        blog.save()
+        return blog
+    
+# Define a schema
+schema = strawberry.schema.Schema(query=Query, mutation=Mutation)
