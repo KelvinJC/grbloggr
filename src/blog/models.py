c52ac93ff17e888ae26fb81ed57e73e2242b14ca
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-04-11 12:11:11 +0100

    Define Post model

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    author = models.CharField(max_length=255, blank=True, null=True)
+    message = models.TextField(blank=True, null=True)
