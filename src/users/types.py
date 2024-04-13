0c84c4acba100f71a0ed8ac991cd4c0b14cf855d
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-04-13 13:17:59 +0100

    Keep all types in one module, to stop cyclic imports between blog.types and user.types

--- src/users/types.py
+++ src/users/types.py
@@ -1,17 +1,27 @@
+from typing import List
 import strawberry
+from . import models
+
 
 @strawberry.type
-class UserType:
+class BlogPostType:
+    id: int
+    title: str
+    author: "UserType1" 
+    message: str
+
+@strawberry.django.type(models.User)
+class UserType1:
     username: str
     email: str
     phone_number: str
     password: strawberry.Private[str]
+    blogposts: List["BlogPostType"]
 
 @strawberry.type
 class LoginSuccess:
-    user: UserType
+    user: UserType1
 
 @strawberry.type
 class LoginError:
     message: str
-
