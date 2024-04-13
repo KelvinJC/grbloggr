31bd321efac1396d76cbb98c240505084de8b9bd
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-04-13 13:16:28 +0100

    Add author as foreign key attribute on BlogPost model

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
 
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=255, blank=True, null=True)
-    author = models.CharField(max_length=255, blank=True, null=True)
     message = models.TextField(blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
